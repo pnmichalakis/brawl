34e3833fa15ab001e30bca449aa96c6c7e16155e
pnmichalakis <pnmichalakis@gmail.com>
2014-12-04 16:45:06 -0500

    implements koala gem, pulling user data

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -7,12 +7,15 @@ ActiveRecord::Base.establish_connection({
 
 set :views, File.expand_path('../../views',__FILE__)
 set :public, File.expand_path('../../public',__FILE__)
-
+require 'koala'
 
 enable :sessions, :method_override
 
 
 get '/' do
+	@graph = Koala::Facebook::API.new(ENV['ACCESSTOKEN'])
+	@person = @graph.get_object("me")
+	binding.pry
 	erb :index
 end
 
