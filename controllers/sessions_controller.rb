ea5dfebd4740db2e55238007a60e03ee7cec3b38
pnmichalakis <pnmichalakis@gmail.com>
2014-12-08 21:57:40 -0500

    adds working logout function

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -2,5 +2,11 @@ class SessionsController < ApplicationController
 	get '/new' do
 		erb :'sessions/new'
 	end
-
+	post '/' do
+  	redirect '/' unless @user = User.find_by({email: params[:user]})
+	end
+	get '/destroy' do
+	  session = nil
+  	redirect '/'
+	end
 end
