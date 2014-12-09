70762710df72972dc53af57db1582e6cbc0cbb2a
pnmichalakis <pnmichalakis@gmail.com>
2014-12-08 23:44:20 -0500

    reconfigures login functionality, no longer 'works' but framework is in place

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -15,38 +15,68 @@ enable :logging, :dump_errors, :raise_errors, :show_exceptions
 enable :sessions, :method_override
 
 	get '/' do
-		@oauth    = Koala::Facebook::OAuth.new(ENV['APPID'], ENV['APPSECRET'], "http://localhost:9292/")
+		# if session_exists
+		# 	user_is_logged_in = true
+		# else
+		# 	user_is_logged_in = false
+		# end
+
+		user_is_logged_in = false
+
+		if user_is_logged_in
+			# app code
+			# swiping and stuff
+
+			erb :index
+		else
+			redirect '/login'
+		end
+	end
+
+
+
+	get '/login' do
+		# prep the 'login with facebook link'
+		@oauth    = Koala::Facebook::OAuth.new(ENV['APPID'], ENV['APPSECRET'], "http://localhost:9292/login")
 		@auth_url = @oauth.url_for_oauth_code
 
+		# authenticate user against facebook
 		if params['code']
 			@code = params['code']
 			@access_token = @oauth.get_access_token(@code)
 			@graph = Koala::Facebook::API.new(@access_token)
 			@person = @graph.get_object("me")
 			@photo = @graph.get_picture("me")
-			@user = User.new
-   	  @user.name = @person["name"]
-  	  @user.picture = @photo
-      @user.email = @person["email"]
-      @user.fbid = @person["id"]
-  	  @user.save!
-			# Create a user in the DB based on facebook credentials
 
-			# Create session
+			# Check if  user exists in the database
+			@user = User.find_by({fbid: params[:user]})
+			if @user != nil
+				# create a session
+				#  <--------------
+				#  <--------------
+			else
+				@user = User.new
+ 	 	 	  @user.name = @person["name"]
+ 	 		  @user.picture = @photo
+  	    @user.email = @person["email"]
+  	    @user.fbid = @person["id"]
+  	    @user.save!
+				# create a session
+				#  <--------------
+				#  <--------------
+			end
 
+			redirect '/'
 		end
 
-
-
-		erb :index
-		# binding.pry
+		erb :login
 	end
+
 
 	get '/logout' do
 		# destroy session
-		redirect '/'
+		redirect '/login'
 	end
-
 end
 
 
--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
 	get '/new' do
 		erb :'sessions/new'
+		redirect '/'
 	end
 	post '/' do
-  	redirect '/' unless @user = User.find_by({email: params[:user]})
+  	redirect '/' unless @user = User.find_by({fbid: params[:user]})
 	end
 	delete '/' do
+		puts 'hellow'
   	session = nil
   	redirect '/'
 	end
